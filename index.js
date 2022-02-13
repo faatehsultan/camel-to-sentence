74b70d4359114b9653bef9cbda38191fcd70490e
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2022-02-13 21:17:35 +0500

    fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default camelToSentence = (camelCaseString) => {
+export const camelToSentence = (camelCaseString) => {
   const result = camelCaseString.replace(/([A-Z])/g, " $1");
   const finalResult = result.charAt(0).toUpperCase() + result.slice(1);
   return finalResult;
